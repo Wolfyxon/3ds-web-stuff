c401c7f2ccee1443452088605145e615cf2f0eca
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-08 22:09:27 +0100

    Fixed lerpColor() not doing correct calculations

--- scripts/lib/anim.js
+++ scripts/lib/anim.js
@@ -1,19 +1,24 @@
-function lerpColor(color, targetColor, amt) {
-    color = parseInt(color);
-    targetColor = parseInt(targetColor);
+function rgbToHex(r, g, b) {
+    return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+}
 
-    const r1 = color >> 16 & 255;
-    const g1 = color >> 8 & 255;
-    const b1 = color & 255;
+function lerpColor(color1, color2, amt) {
+    color1 = color1.replace("#","");
+    color2 = color2.replace("#","");
 
-    const r2 = targetColor >> 16 & 255;
-    const g2 = targetColor >> 8 & 255;
-    const b2 = targetColor & 255;
+    const r1 = parseInt(color1.substring(1, 3), 16);
+    const g1 = parseInt(color1.substring(3, 5), 16);
+    const b1 = parseInt(color1.substring(5, 7), 16);
 
-    const lerpedR = Math.round(lerp(r1, r2, amt));
-    const lerpedG = Math.round(lerp(g1, g2, amt));
-    const lerpedB = Math.round(lerp(b1, b2, amt));
+    const r2 = parseInt(color2.substring(1, 3), 16);
+    const g2 = parseInt(color2.substring(3, 5), 16);
+    const b2 = parseInt(color2.substring(5, 7), 16);
 
+    const r = Math.round(lerp(r1,r2,amt));
+    const g = Math.round(lerp(g1,g2,amt));
+    const b = Math.round(lerp(b1,b2,amt));
 
-    return (lerpedR << 16) | (lerpedG << 8) | lerpedB;
+    const hex = rgbToHex(r,g,b)
+    console.log(hex)
+    return hex
 }
