e27b5bd29c04784dbf4dbc98f58e7c134bceb6fd
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-08 09:51:27 +0100

    Code execution

--- scripts/jsEditor.js
+++ scripts/jsEditor.js
@@ -2,4 +2,12 @@
 window.addEventListener("load",function(){
     const editor = document.getElementById("editor");
     editor.value = "alert(\"Hello World!\")"
+
+    document.getElementById("btn-run").addEventListener("click",function(){
+        try{
+            eval(editor.value.replace("\n",";\n")+";");
+        } catch (e){
+            alert(e.line | e.lineNumber)
+        }
+    })
 })
