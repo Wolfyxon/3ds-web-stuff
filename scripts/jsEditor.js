f8f7eb61f58b82a2fb405604bc483bb9d7c9c132
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-08 09:04:29 +0100

    Removed highlight.js and trying to figure it out with Ace

--- scripts/jsEditor.js
+++ scripts/jsEditor.js
@@ -1,8 +1,12 @@
-window.addEventListener("load",function(){
-    const code = document.getElementById("code");
-    code.value = "alert(\"Hello World\")"
 
-    document.getElementById("btn-run").addEventListener("click",function(){
-        eval(code.value);
+window.addEventListener("load",function(){
+    const editor = ace.edit(document.getElementById("code-input"), {
+        mode: "ace/mode/javascript",
+        selectionStyle: "text"
     })
+    editor.setOptions({
+        autoScrollEditorIntoView: true,
+        copyWithEmptySelection: true,
+    });
+
 })
