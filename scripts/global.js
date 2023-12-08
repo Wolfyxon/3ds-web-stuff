30e1d13b6945e4fec19a04bfa2a33c3383ad3239
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-08 20:57:52 +0100

    Fixed randi() procuding weird results

--- scripts/global.js
+++ scripts/global.js
@@ -5,7 +5,7 @@ function randf(min, max) {
 function randi(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function lerp(start, end, amt){
