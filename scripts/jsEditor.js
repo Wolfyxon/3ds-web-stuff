aa858ef009c0b9bc99558a59817bb118196134cf
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-08 09:20:43 +0100

    Removed syntax highlighting libs as they don't work on the 3DS

--- scripts/jsEditor.js
+++ scripts/jsEditor.js
@@ -1,12 +1,4 @@
 
 window.addEventListener("load",function(){
-    const editor = ace.edit(document.getElementById("code-input"), {
-        mode: "ace/mode/javascript",
-        selectionStyle: "text"
-    })
-    editor.setOptions({
-        autoScrollEditorIntoView: true,
-        copyWithEmptySelection: true,
-    });
 
 })
