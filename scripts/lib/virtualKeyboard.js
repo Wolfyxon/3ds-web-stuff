c556c8ae68979443deeb8dbb1a5b13b51fe29cc5
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-08 20:21:45 +0100

    Added erase()

--- scripts/lib/virtualKeyboard.js
+++ scripts/lib/virtualKeyboard.js
@@ -73,6 +73,14 @@ function createKeyboard(element) {
         elm.setSelectionRange(cursorPos + 1, cursorPos + 1);
     }
 
+    function erase(){
+        const elm = kb.focusedElement;
+        const cursorPos = elm.selectionStart;
+        if(cursorPos < 1) return;
+        const current = elm.value;
+        elm.value = current.slice(0, cursorPos-1)
+        elm.setSelectionRange(cursorPos - 1, cursorPos - 1);
+    }
 
     function setCaps(enabled){
         kb.caps = enabled
