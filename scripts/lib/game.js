1f4b6e68e24be9f2afb0e5d382d4af9e39ad6613
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-09 16:08:38 +0100

    Added isTouching()

--- scripts/lib/game.js
+++ scripts/lib/game.js
@@ -17,6 +17,20 @@ function Vector2(x=0,y=0){
     return vec;
 }
 
+function isTouching(x1, y1, w1, h1, x2, y2, w2=w1, h2=h1) {
+    const left1 = x1;
+    const right1 = x1 + w1;
+    const top1 = y1;
+    const bottom1 = y1 + h1;
+
+    const left2 = x2;
+    const right2 = x2 + w2;
+    const top2 = y2;
+    const bottom2 = y2 + h2;
+
+    return !(left1 > right2 || right1 < left2 || top1 > bottom2 || bottom1 < top2);
+}
+
 function distanceToXY(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
 }
