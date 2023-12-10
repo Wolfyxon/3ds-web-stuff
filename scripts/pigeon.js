f4b6beea3758cd8e8bdb04e523286622468e6bdc
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-10 23:19:53 +0100

    Added rendering

--- scripts/pigeon.js
+++ scripts/pigeon.js
@@ -2,6 +2,11 @@ window.addEventListener("load",function(){
     const canvas = document.getElementById("canv");
     const ctx = canvas.getContext("2d");
 
+    const imgWingUp = document.getElementById("img-pigeon1");
+    const imgWingDown = document.getElementById("img-pigeon2")
+
+    const pigeon = Sprite(imgWingUp,10,90)
+
     var passedPipes = 0;
     var highScore = 0;
 
@@ -10,4 +15,17 @@ window.addEventListener("load",function(){
     function addPipes(y){
 
     }
+
+    function switchSprite(){
+        if(pigeon.image === imgWingUp) pigeon.image = imgWingDown;
+        else pigeon.image = imgWingUp;
+    }
+
+    setInterval(switchSprite,200)
+
+    // Main loop
+    setInterval(function(){
+        clearCanvas(canvas);
+        pigeon.render(canvas);
+    })
 })
