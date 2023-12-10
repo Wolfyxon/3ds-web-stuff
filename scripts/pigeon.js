a648e860c6f4c36bcbed222e9fefa19f86676902
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-10 19:23:38 +0100

    Prepared script

---
+++ scripts/pigeon.js
@@ -0,0 +1,13 @@
+window.addEventListener("load",function(){
+    const canvas = document.getElementById("canv");
+    const ctx = canvas.getContext("2d");
+
+    var passedPipes = 0;
+    var highScore = 0;
+
+    var pipes = []
+
+    function addPipes(y){
+
+    }
+})
