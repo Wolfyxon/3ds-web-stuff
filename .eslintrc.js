d29ac3a8dd66c0e21b04a7b136cdf97ec01ee5fd
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-11 16:00:24 +0100

    Disallowed 'let'

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,12 @@ module.exports = {
     rules: {
         'func-style': ['error', 'declaration'], // disallow lambda expressions
         'prefer-const': 'error', // Force const for variables that never change
+        'no-restricted-syntax': [
+            'error',
+            {
+                selector: 'VariableDeclaration[kind="let"]',
+                message: '"let" is not supported on the 3DS browser.',
+            },
+        ],
     },
 };
