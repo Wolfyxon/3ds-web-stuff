ba0c7b05805c4dd9d4867d15e9a5b47a3b30137f
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-24 22:23:49 +0100

    Added fmod()

--- scripts/global.js
+++ scripts/global.js
@@ -12,6 +12,10 @@ function lerp(start, end, weight){
     return (1-weight)*start+weight*end
 }
 
+function fmod(a, b){
+    return Number((a - (Math.floor(a / b) * b)).toPrecision(8));
+}
+
 function colorNameToHex(colour) {
     const colours = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
         "beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887",
