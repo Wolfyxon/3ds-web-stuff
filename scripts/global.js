e2597126c263554ef647f0f0919480061e507e7d
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-02 15:47:28 +0100

    Added httpGet()

--- scripts/global.js
+++ scripts/global.js
@@ -45,6 +45,26 @@ function colorNameToHex(colour) {
     if (typeof colours[colour.toLowerCase()] != 'undefined') return colours[colour.toLowerCase()];
 }
 
+function httpGet(url, callback){
+    if(!url){
+        console.error("No URL specified");
+        return;
+    }
+    if(!callback){
+        console.error("No callback function specified");
+        return;
+    }
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) callback(xhr.status, xhr.responseText);
+    };
+
+    xhr.open('GET', url, true);
+    xhr.send();
+}
+
 function is3DS(){
     return includes(window.navigator.userAgent,"Nintendo 3DS") && getGamepad();
 }
