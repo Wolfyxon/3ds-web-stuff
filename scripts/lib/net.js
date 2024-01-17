44201d61c1f5658ec30120956681ab19c2900a3a
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-17 22:01:08 +0100

    Added httpPost()

--- scripts/lib/net.js
+++ scripts/lib/net.js
@@ -17,3 +17,27 @@ function httpGet(url, callback){
     xhr.open('GET', url, true);
     xhr.send();
 }
+
+function httpPost(url, data, callback, contentType) {
+    if(!url) {
+        console.error("No URL specified");
+        return;
+    }
+    if(!callback){
+        console.error("No callback function specified");
+        return;
+    }
+    if(!contentType) contentType = "application/json";
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === 4) callback(xhr.status, xhr.responseText);
+    };
+
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader("Content-Type", contentType);
+
+    const jsonData = JSON.stringify(data);
+    xhr.send(jsonData);
+}
