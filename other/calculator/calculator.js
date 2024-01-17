c149c27405f843a97b506bc7e2cb36187b4b714d
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-17 23:33:14 +0100

    Added pads logic

--- other/calculator/calculator.js
+++ other/calculator/calculator.js
@@ -1,3 +1,17 @@
 window.addEventListener("load", function(){
+    const inputBtns = document.getElementById("pads").getElementsByTagName("button");
 
+    const formula = document.getElementById("formula");
+    const result = document.getElementById("result");
+
+    function registerInputBtn(button){
+        if(button.innerText === "=") return;
+        button.addEventListener("click",function(){
+            formula.innerText += button.innerText;
+        })
+    }
+
+    for(var i=0; i<inputBtns.length; i++){
+        registerInputBtn(inputBtns[i]);
+    }
 });
