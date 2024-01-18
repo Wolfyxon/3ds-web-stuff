313aa7f57664360982e1969f11248514619705e0
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-18 17:21:51 +0100

    Another form can't be submitted if one is already processing

--- scripts/contact.js
+++ scripts/contact.js
@@ -7,9 +7,11 @@ window.addEventListener("load",function(){
     const contentError = document.getElementById("content-error");
 
     var submitted = false;
+    var processing = false;
 
     form.onsubmit = function(){
         if(submitted) return false;
+        if(processing) return false;
         const content = contentInput.value;
 
         if(content.replace(" ","") === ""){
@@ -24,11 +26,14 @@ window.addEventListener("load",function(){
         var email = emailInput.value;
         if(email.replace(" ","") === "") email = "No email specified";
 
+        processing = true;
+
         const url = "https://discord.com/api/webhooks/1197557152047444111/o0jJdbX2BFgE2v6-mjy51HfLTEwPW2g6qIZXsYvERAmKXQDbwdUPDDulftGFg6f2ieoU";
         httpPost(url,{
             username: email,
             content: content
         },function(code){
+            processing = false;
             if(code === 204){
                 alert("Form submitted successfully! \nIf you specified an email, you should get a reply in up to 24 hours.\nReturning to the home page.");
                 window.location.href = "index.html";
