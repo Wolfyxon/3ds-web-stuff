925a333504f08fa7dc7e2996e94ce054072c806d
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-19 10:24:11 +0100

    User can now chose if the number has to be a integer or a float

--- other/rand/rand.js
+++ other/rand/rand.js
@@ -2,8 +2,12 @@ window.addEventListener("load",function(){
     const minInput = document.getElementById("min");
     const maxInput = document.getElementById("max");
     const resultTxt = document.getElementById("result");
+    const intChk = document.getElementById("chk-int");
 
     document.getElementById("btn-gen").addEventListener("click",function(){
-        resultTxt.innerText = randi(minInput.value, maxInput.value);
+        var res = randf(minInput.value, maxInput.value);
+        if(intChk.checked) res = Math.floor(res);
+
+        resultTxt.innerText = res;
     });
 });
