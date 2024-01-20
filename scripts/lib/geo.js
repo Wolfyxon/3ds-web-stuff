487b1e5ea593d00481016a058db962f8b9e97dd2
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-20 17:12:22 +0100

    Init geo.js library

---
+++ scripts/lib/geo.js
@@ -0,0 +1,20 @@
+// This library utilizes the free public OpenMeteo API
+// https://open-meteo.com/
+
+if(!httpGet) throw new Error("net.js not imported!")
+
+/***
+ * Searches for real life locations with the given query and returns the results to the callback function
+ * @param {String} query
+ * @param {Function} callback Callback function called with the result array
+ */
+function searchLocations(query, callback){
+    const url = "https://geocoding-api.open-meteo.com/v1/search?name="
+    httpGet(url+query, function(code, body){
+        const jsonBody = JSON.parse(body);
+        var results = [];
+        const gottenResults = jsonBody["results"];
+        if(gottenResults) results = gottenResults;
+        callback(results);
+    });
+}
