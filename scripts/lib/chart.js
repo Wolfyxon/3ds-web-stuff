91e3ecbd931ff1d05a6acf79cf4beb9a142d8782
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-21 20:37:26 +0100

    Fixed canvas being cleared when multiple charts are drawn

--- scripts/lib/chart.js
+++ scripts/lib/chart.js
@@ -11,8 +11,10 @@ function drawLineChart(canvas, points, color) {
     const h = canvas.clientHeight;
 
     // Ensure the canvas isn't stretched;
-    canvas.width =  w;
-    canvas.height = h;
+    if(canvas.width !== w || canvas.height !== h){
+        canvas.width =  w;
+        canvas.height = h;
+    }
 
     var minY = 0;
     var maxY = 0;
