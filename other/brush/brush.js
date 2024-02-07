b1a77ae5238efc81cc27a00fab3fdc6d41e8d86d
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-02-07 12:30:16 +0100

    Fixed inability to draw using the touch screen

--- other/brush/brush.js
+++ other/brush/brush.js
@@ -4,16 +4,33 @@ window.addEventListener("load", function(){
 
     var brushSize = 10;
 
-    function draw(e) {
+    function draw(x, y) {
+        ctx.fillRect(x - brushSize/2, y - brushSize/2, brushSize, brushSize);
+    }
+
+    function drawMouse(e) {
         if(e.buttons === 0) return;
         const rect = canvas.getBoundingClientRect();
         const scaleX = canvas.width / rect.width;
         const scaleY = canvas.height / rect.height;
         const x = (e.clientX - rect.left) * scaleX;
         const y = (e.clientY - rect.top) * scaleY;
-        ctx.fillRect(x - brushSize/2, y - brushSize/2, brushSize, brushSize);
+        draw(x,y);
     }
 
-    canvas.addEventListener("mousemove", draw);
-    canvas.addEventListener("mousedown",draw);
+    function drawTouch(e) {
+        e.preventDefault();
+        const rect = canvas.getBoundingClientRect();
+        const scaleX = canvas.width / rect.width;
+        const scaleY = canvas.height / rect.height;
+        const touch = e.touches[0];
+        const x = (touch.clientX - rect.left) * scaleX;
+        const y = (touch.clientY - rect.top) * scaleY;
+        draw(x,y);
+    }
+
+    canvas.addEventListener("mousemove", drawMouse);
+    canvas.addEventListener("touchmove",drawTouch);
+    canvas.addEventListener("mousedown", drawMouse);
+
 });
