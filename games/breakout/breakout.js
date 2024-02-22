8a4f8f1e1dd2f3f7058c2216c322465e479f9913
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-02-22 14:05:16 +0100

    Lowered ball speed

--- games/breakout/breakout.js
+++ games/breakout/breakout.js
@@ -10,7 +10,7 @@ window.addEventListener("load", function() {
 
     const ballSize = 4;
     const ballAccel = 1.02;
-    const ballInitSpeed = 0.5;
+    const ballInitSpeed = 0.15;
     var ballSpeed = ballInitSpeed;
     const ball = Rect2D(Vector2(canvas.width/2 - ballSize/2,canvas.height/2 - ballSize/2),ballSize, ballSize);
     const initBallPos = ball.area.startVec.copy();
