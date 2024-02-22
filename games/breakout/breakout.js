d4e147d94eaee0b39fa580debf058b962f89bc91
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-02-22 08:53:15 +0100

    Decreased ball acceleration

--- games/breakout/breakout.js
+++ games/breakout/breakout.js
@@ -9,7 +9,7 @@ window.addEventListener("load", function() {
     player.fillStyle = "#8C7BFF";
 
     const ballSize = 4;
-    const ballAccel = 1.06;
+    const ballAccel = 1.02;
     const ballInitSpeed = 0.5;
     var ballSpeed = ballInitSpeed;
     const ball = Rect2D(Vector2(canvas.width/2 - ballSize/2,canvas.height/2 - ballSize/2),ballSize, ballSize);
