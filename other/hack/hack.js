e6448f9680de27f82b5a842ba35be846f154bfd9
Wolfyxon <wolfyxon@gmail.com>
2024-04-01 12:50:37 +0200

    Init fake hacking interface

---
+++ other/hack/hack.js
@@ -0,0 +1,48 @@
+const terminalTexts = [
+    "rm -rf /* --no-preserve-root",
+    "pacman -Syu",
+    "sudo -s",
+    "neofetch",
+    "Hacking database",
+    "Establishing connection",
+    "Hi",
+    "undefined",
+    "Hello World!",
+    "Goodbye World!",
+    "Ah free at last",
+    "A visitor. Hm indeed, I've slept long enough.",
+    "Rise and shine mr Freeman",
+    "Get out",
+    "kernel panic - not syncing: Attempted to kill inint !",
+    "Uncaught ReferenceError: message is not defined",
+    "Exception in thread \"main\" java.lang.NullPointerException",
+    "Segmentation fault (core dumped)",
+    "Fatal error: Unexpectedly found nil while unwrapping an Optional value",
+    "ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'WHERE id=1' at line 1",
+    "System.NullReferenceException: Object reference not set to an instance of an object.",
+    "panic: runtime error: index out of range [5] with length 3",
+    "Type mismatch: inferred type is Int but String was expected",
+    "thread 'main' panicked at 'attempt to add with overflow', src/main.rs:4:9",
+    "for(int i = 0; i < sizeof(arr); i++) {}",
+]
+
+window.addEventListener("load", function() {
+    const terminal = document.getElementById("terminal");
+
+    function echo(text) {
+        const line = document.createElement("div");
+        line.innerText = text;
+
+        if(terminal.children.length > 15) {
+            terminal.removeChild( terminal.children[0] );
+        }
+
+        terminal.appendChild(line);
+    }
+
+    function terminalLoop() {
+        echo( terminalTexts[randi(0, terminalTexts.length-1)] );
+        setTimeout(terminalLoop, randi(1,300));
+    }
+    terminalLoop();
+});
