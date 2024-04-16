3acca47be8fad0a76769a8cf98f3bd5392763460
Wolfyxon <wolfyxon@gmail.com>
2024-04-16 23:11:02 +0200

    Added SceneNode

--- scripts/lib/elementGame.js
+++ scripts/lib/elementGame.js
@@ -21,4 +21,15 @@ function Scene(element) {
 }
 Scene.prototype = Object.create(Node.prototype);
 
+/**
+ * Scene node used for elements such as sprites
+ * @param {Vector2} position
+ * @constructor
+ */
+function SceneNode(position) {
+    this.rotation = 0;
+    this.position = position || new Vector2(0, 0);
+}
+SceneNode.prototype = Object.create(Node.prototype);
+
 // TODO: Implement the rest
