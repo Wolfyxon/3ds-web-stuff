f27c6f34a9dfde3a1bc7a36433484ba469437243
Wolfyxon <wolfyxon@gmail.com>
2024-04-16 22:58:14 +0200

    Init elementGame

---
+++ scripts/lib/elementGame.js
@@ -0,0 +1,12 @@
+// Alternative to canvasGame that utilizes DOM elements instead of drawing on canvases
+
+/**
+ * Scene constructor
+ * @param {HTMLElement} element Container element of the scene
+ * @constructor
+ */
+function Scene(element) {
+    this.element = element;
+}
+
+// TODO: Implement the rest
