78385deed0a5c41c32fc7791061284c620d6ea5c
Wolfyxon <wolfyxon@gmail.com>
2024-04-16 23:07:55 +0200

    Added Vector2

--- scripts/lib/elementGame.js
+++ scripts/lib/elementGame.js
@@ -1,6 +1,17 @@
 // Alternative to canvasGame that utilizes DOM elements instead of drawing on canvases
 
 /**
+ * 2D position
+ * @param {number} x
+ * @param {number} y
+ * @constructor
+ */
+function Vector2(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+/**
  * Scene constructor
  * @param {HTMLElement} element Container element of the scene
  * @constructor
