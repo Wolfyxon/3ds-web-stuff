6872ca4ba591e368a0f938ce407c5ca7b06269c1
Wolfyxon <wolfyxon@gmail.com>
2024-04-18 16:26:29 +0200

    Reorganize

--- scripts/lib/elementGame.js
+++ scripts/lib/elementGame.js
@@ -3,6 +3,16 @@
 libName("elementGame");
 depend("gameBase");
 
+
+/**
+ * Scene node used for elements such as sprites
+ * @param {HTMLElement} element Target element
+ * @constructor
+ */
+function SceneNode(element) {
+    this.element = element;
+}
+
 /**
  * Scene constructor
  * @param {HTMLElement} element Container element of the scene
@@ -11,17 +21,7 @@ depend("gameBase");
 function Scene(element) {
     this.element = element;
 }
-Scene.prototype = Object.create(Node.prototype);
+Scene.prototype = Object.create(SceneNode.prototype);
 
-/**
- * Scene node used for elements such as sprites
- * @param {Vector2} position
- * @constructor
- */
-function SceneNode(position) {
-    this.rotation = 0;
-    this.position = position || new Vector2(0, 0);
-}
-SceneNode.prototype = Object.create(Node.prototype);
 
 // TODO: Implement the rest
