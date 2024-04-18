d5977a2adde699ac165476f7e2028cfa977a349b
Wolfyxon <wolfyxon@gmail.com>
2024-04-18 16:39:44 +0200

    Initial implementation

--- scripts/lib/elementGame.js
+++ scripts/lib/elementGame.js
@@ -11,6 +11,35 @@ depend("gameBase");
  */
 function SceneNode(element) {
     this.element = element;
+    element.style.position = "absolute";
+
+    this._rotation = 0;
+    this._pos = new Vector2(0, 0);
+}
+SceneNode.prototype = {
+    getPosition: function() {
+        return this._pos;
+    },
+
+    getRotation: function() {
+        return this.rotation;
+    },
+
+    setRotation: function(deg) {
+        this._rotation = deg;
+        this.updateTransform();
+    },
+
+    updateTransform: function() {
+        const x = this._pos.x + "px";
+        const y = this._pos.y + "px";
+        var trStr = "rotate(" + this._rotation + "deg)";
+
+        this.element.style.webkitTransform = trStr;
+        this.element.style.transform = trStr;
+        this.element.style.left = x;
+        this.element.style.top = y;
+    }
 }
 
 /**
@@ -20,6 +49,7 @@ function SceneNode(element) {
  */
 function Scene(element) {
     this.element = element;
+    element.style.position = "relative";
 }
 Scene.prototype = Object.create(SceneNode.prototype);
 
