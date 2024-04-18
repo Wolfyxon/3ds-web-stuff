774d2cca78e26e3d7bc17192e6757502ee69e3c5
Wolfyxon <wolfyxon@gmail.com>
2024-04-18 15:34:39 +0200

    Recreated Vector2

--- scripts/lib/gameBase.js
+++ scripts/lib/gameBase.js
@@ -11,6 +11,200 @@ This library is for basic things, for more advanced features see: canvasGame and
  */
 libName("gameBase");
 
+/////////  CLASSES  /////////
+
+/**
+ * 2D position
+ * @param {number} x
+ * @param {number} y
+ * @constructor
+ */
+function Vector2(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Vector2.prototype = {
+    /**
+     * Offsets the vector by X and Y values
+     * @param {Number} x Horizontal offset
+     * @param {Number} y Vertical offset
+     * @return {Vector2}
+     */
+    offsetXY: function (x, y) {
+        this.x += x;
+        this.y += y;
+        return this;
+    },
+
+    /**
+     * Returns an offset copy of the vector by X and Y values
+     * @param {Number} x Horizontal offset
+     * @param {Number} y Vertical offset
+     * @return {Vector2}
+     */
+    getOffsetXY: function(x, y) {
+        return this.copy().offsetXY(x, y);
+    },
+
+    /**
+     * Returns an offset copy of the vector
+     * @param {Vector2} vec Offset vector
+     * @return {Vector2}
+     */
+    getOffsetVec: function(vec) {
+        return this.copy().offsetVec(vec);
+    },
+
+    /**
+     * Offsets the vector by another Vector2
+     * @param {Vector2} vec Offset vector
+     * @return {Vector2}
+     */
+    offsetVec: function (vec) {
+        return vec.offsetXY(vec.x, vec.y);
+    },
+
+    /**
+     * Offsets the vector respecting the given rotation
+     * @param {Number} x X amount to offset
+     * @param {Number} y Y amount to offset
+     * @param {Number} rotation Rotation in degrees
+     * @returns {Vector2}
+     */
+    offsetRotatedXY: function(x, y, rotation) {
+        const angle = deg2rad(rotation);
+        const localX = x * Math.cos(angle) - y * Math.sin(angle);
+        const localY = x * Math.sin(angle) + y * Math.cos(angle);
+
+        return this.offsetXY(localX, localY);
+    },
+
+    /**
+     * Offsets the vector respecting the given rotation
+     * @param {Vector2} vec Offset vector
+     * @param {Number} rotation Rotation in degrees
+     */
+    offsetRotatedVec: function (vec, rotation) {
+        return this.offsetRotatedXY(vec.x, vec.y, rotation);
+    },
+
+    /**
+     * Performs a linear interpolation to the X and Y values
+     * @param {Number} x Horizontal position
+     * @param {Number} y Vertical position
+     * @param {Number} weight Speed of the interpolation
+     * @return {Vector2}
+     */
+    lerpXY: function (x, y, weight) {
+        this.x = lerp(this.x, x, weight);
+        this.y = lerp(this.y, y, weight);
+
+        return this;
+    },
+
+    /**
+     * Performs a linear interpolation to the target Vector2
+     * @param {Vector2} vec The target Vector2
+     * @param {Number} weight Speed of the interpolation
+     * @return {Vector2}
+     */
+    lerpVec: function(vec, weight) {
+        return this.lerpXY(vec.x, vec.y, weight);
+    },
+
+    /**
+     * Returns a linearly interpolated copy of the vector by the X and Y values
+     * @param {Number} x Horizontal position
+     * @param {Number} y Vertical position
+     * @param {Number} weight Speed of the interpolation
+     * @return {Vector2}
+     */
+    getLerpedXY: function(x, y, weight) {
+        return this.copy().lerpXY(x, y, weight);
+    },
+
+    /**
+     * Returns a linearly interpolated copy of the vector to the target Vector2
+     * @param {Vector2} vec The target Vector2
+     * @param {Number} weight Speed of the interpolation
+     * @return {Vector2}
+     */
+    getLerpedVec: function(vec, weight) {
+        return this.getLerpedXY(vec.x, vec.y, weight);
+    },
+
+    /**
+     * Kept for backwards compatibility, use lerpVec()
+     * @deprecated
+     */
+    lerp: function(vec, weight) {
+        return this.lerpVec(vec, weight);
+    },
+
+    /**
+     * Kept for backwards compatibility, use getLerpedVec()
+     * @deprecated
+     */
+    getLerped: function(vec, weight) {
+        return this.getLerpedVec(vec, weight);
+    },
+
+    /**
+     * Calculates an angle in degrees to the target point at X and Y values
+     * @param {Number} x Horizontal position
+     * @param {Number} y Vertical position
+     * @return {Object}
+     */
+    getRotationToXY: function(x, y) {
+        const deltaX = x - this.x;
+        const deltaY = y - this.y;
+        return rad2deg( Math.atan2(deltaY, deltaX) );
+    },
+
+    /**
+     * Calculates an angle in degrees to the target Vector2
+     * @param {Vector2} vec Target Vector2
+     * @return {number}
+     */
+    getRotationToVec: function(vec) {
+        return this.getRotationToXY(vec.x, vec.y);
+    },
+
+    /**
+     * Moves the vector towards the target point at X and Y values
+     * @param {Number} x Horizontal position
+     * @param {Number} y Vertical position
+     * @param {Number} amt Speed of the movement
+     * @return {Vector2}
+     */
+    moveTowardXY: function(x, y, amt) {
+        const angle = Math.atan2(y - this.y, x - this.x);
+        this.x += Math.cos(angle) * amt;
+        this.y += Math.sin(angle) * amt;
+        return this;
+    },
+
+    /**
+     * Moves the vector towards tge target Vector2
+     * @param {Vector2} vec Target Vector2
+     * @param {Number} amt Speed of the movement
+     * @return {Vector2}
+     */
+    moveTowardVec: function(vec, amt) {
+        return this.moveTowardXY(vec.x, vec.y, amt);
+    },
+
+    /**
+     * Returns an independent copy of this vector
+     * @return {Vector2}
+     */
+    copy: function() {
+        return new Vector2(this.x, this.y);
+    }
+}
+
+///////// FUNCTIONS /////////
+
 /**
  * Performs a linear interpolation between 2 angles in degrees
  * @param {Number} startDeg Current angle
