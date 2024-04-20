95c3d9fea8d159df2d496c0121fd0a8bcb4bc38c
Wolfyxon <wolfyxon@gmail.com>
2024-04-20 12:03:07 +0200

    Fixed chicken image path

--- games/elmGameTest/elmGameTest.js
+++ games/elmGameTest/elmGameTest.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function(){
     const scene = new Scene(document.getElementById("scene"));
     const fpsTxt = document.getElementById("fps");
 
-    const chicken = new Sprite("../../../img/pets/chicken/chicken1.png");
+    const chicken = new Sprite("../../img/pets/chicken/chicken1.png");
     scene.addChild(chicken);
 
     const maxwell = new Sprite("../../img/pets/maxwell_trim.png");
