736f6076de89ea26648c021eb781ca6c329eedaf
Wolfyxon <wolfyxon@gmail.com>
2024-04-20 12:26:33 +0200

    Documented all SceneNode functions

--- scripts/lib/elementGame.js
+++ scripts/lib/elementGame.js
@@ -15,6 +15,9 @@ function SceneNode(element) {
 }
 SceneNode.prototype = {
 
+    /**
+     * Initializes the node. Called by constructors.
+     */
     init: function() {
         this._parent = null;
         this._children = [];
@@ -26,42 +29,79 @@ SceneNode.prototype = {
         this.updateTransform();
     },
 
+    /**
+     * Sets the node's position to a Vector2
+     * @param {Vector2} vec
+     */
     setPositionVec: function(vec) {
         this._pos = vec;
         this.updateTransform();
     },
 
+    /**
+     * Sets the node's position to X and Y values
+     * @param {number} x Horizontal position
+     * @param {number} y Vertical position
+     */
     setPositionXY: function(x, y) {
         this._pos.x = x;
         this._pos.y = y;
         this.updateTransform();
     },
 
+    /**
+     * Moves the node by the specified Vector2
+     * @param {Vector2} vec
+     */
     moveVec: function(vec) {
         this.moveXY(vec.x, vec.y);
     },
 
+    /**
+     * Moves the node by X and Y values
+     * @param {number} x Horizontal position
+     * @param {number} y Vertical position
+     */
     moveXY: function(x, y) {
         this.setPositionXY(this._pos.x + x, this._pos.y + y);
     },
 
+    /**
+     * Returns the node's Vector2 position
+     * @return {Vector2}
+     */
     getPosition: function() {
         return this._pos;
     },
 
+    /**
+     * Returns the node's rotation in degrees
+     * @return {number}
+     */
     getRotation: function() {
         return this.rotation;
     },
 
+    /**
+     * Sets the node's rotation in degrees
+     * @param {number} deg
+     */
     setRotation: function(deg) {
         this._rotation = deg;
         this.updateTransform();
     },
 
+    /**
+     * Rotates the node by the specified degrees
+     * @param {number} deg
+     */
     rotate: function(deg) {
         this.setRotation(this._rotation + deg);
     },
 
+    /**
+     * Updates the rendered node's position, rotation and size
+     */
     updateTransform: function() {
         const x = this._pos.x + "px";
         const y = this._pos.y + "px";
@@ -73,12 +113,20 @@ SceneNode.prototype = {
         this.element.style.top = y;
     },
 
+    /**
+     * Adds a child to the node
+     * @param {SceneNode} node
+     */
     addChild: function(node) {
         node._parent = this;
         this._children.push(node);
         this.element.appendChild(node.element);
     },
 
+    /**
+     * Removes a child from the node
+     * @param {SceneNode} node
+     */
     removeChild: function(node) {
         const idx = this._children.indexOf(node);
         if(idx === -1) {
@@ -91,14 +139,25 @@ SceneNode.prototype = {
         node.remove();
     },
 
+    /**
+     * Returns an array of the node's children
+     * @return {[SceneNode]}
+     */
     getChildren: function() {
         return this._children;
     },
 
+    /**
+     * Returns the node's parent node
+     * @return {SceneNode|null}
+     */
     getParent: function() {
         return this._parent;
     },
 
+    /**
+     * Removes the node
+     */
     remove: function() {
         const par = this.getParent();
 
