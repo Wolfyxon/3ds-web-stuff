18ea568590339c12cfcafea803d478e53ba20023
magiczocker10 <59317952+magiczocker10@users.noreply.github.com>
2024-04-20 14:21:33 +0200

    Fix issues: * The death screen doesn't have a black transparent background (should be also animated if possible) * Player's position doesn't reset after respawning * Projectiles are very small * Firing is slow * Player can only move in 1 direction at a time (diagonal movement is very important) * Player is now too slow * Projectiles are very slow

--- games/jetfighter/jetfighter.js
+++ games/jetfighter/jetfighter.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function() {
 	const heliEle = document.getElementById('helis'),
 		screenEle = document.getElementById('screen'),
+		overlay = document.getElementById('overlay'),
 		targetEle = document.getElementById('target'),
 		projectiles = document.getElementById('projectiles'),
 		lifebar = document.getElementById('lifebar-neg'),
@@ -8,7 +9,7 @@ window.addEventListener('load', function() {
 		gameover = document.getElementById('gameover'),
 		screenCoords = screenEle.getBoundingClientRect(),
 		degreeInRadiant = 2 * Math.PI / 360,
-		speed = 2;
+		speed = 5;
 	var helis = [],
 		running = true,
 		prevMoveFrameTime = 0,
@@ -46,12 +47,11 @@ window.addEventListener('load', function() {
 					(targetCoords.top + targetCoords.height * 0.5) - (coords.top + coords.height * 0.5),
 					(targetCoords.left + targetCoords.width * 0.5) - (coords.left + coords.width * 0.5)
 				) * 180 / Math.PI) + 90;
-			helis[i][2].dataset.rot = a;
+			helis[i][2].setAttribute('data-rot', a);
 			helis[i][2].style.transform = 'rotate(' + a + 'deg)';
 			helis[i][2].style.webkitTransform = 'rotate(' + a + 'deg)';
 		}
 	}
-	setInterval(f, 10);
 
 	function spawnProjectile() {
 		if (!running) {return;}
@@ -63,18 +63,18 @@ window.addEventListener('load', function() {
 
 			// Move the projectile 65 steps towards rotation.
 			// This should prevent spawning directly inside of the heli.
-			var angle = degreeInRadiant * (Number(heli[2].dataset.rot) - 90);
+			var angle = degreeInRadiant * (Number(heli[2].getAttribute('data-rot')) - 90);
 			var deltaX = 65 * Math.cos(angle);
 			var deltaY = 65 * Math.sin(angle);
 			e.style.left = (parseFloat(coords.left) + parseFloat(coords.width) * 0.5 + deltaX) + 'px';
 			e.style.top = (parseFloat(coords.top) + parseFloat(coords.height) * 0.5 + deltaY) + 'px';
 			e.style.transform = 'rotate(' + heli[2].getAttribute('data-rot') + 'deg)';
 			e.style.webkitTransform = 'rotate(' + heli[2].getAttribute('data-rot') + 'deg)';
-			e.dataset.rot = heli[2].getAttribute('data-rot');
+			e.setAttribute('data-rot', heli[2].getAttribute('data-rot'));
 			projectiles.appendChild(e);
 		}
 	}
-	setInterval(spawnProjectile, 3000);
+
 	function spawnProjectile2() {
 		if (!running) {return;}
 		var coords = getComputedStyle(targetEle);
@@ -82,20 +82,46 @@ window.addEventListener('load', function() {
 		e.className = 'jet projectile';
 		e.style.left = (parseFloat(coords.left) + parseFloat(coords.width) * 0.5) + 'px';
 		e.style.top = (parseFloat(coords.top) - 10) + 'px';
-		e.dataset.rot = targetEle.dataset.rot;
+		e.setAttribute('data-rot', targetEle.getAttribute('data-rot'));
 		projectiles.appendChild(e);
 	}
+
+	function reset() {
+		prevMoveFrameTime = Date.now();
+		prevMainFrameTime = Date.now();
+		screenEle.style.webkitAnimationPlayState = 'running';
+		screenEle.style.animationPlayState = 'running';
+		gameover.style.display = 'none';
+		killCounter.innerText = '0';
+		projectiles.innerText = '';
+		heliEle.innerText = '';
+		lifebar.style.height = '0%';
+		targetEle.style.left = '';
+		targetEle.style.top = '';
+		targetEle.setAttribute('data-health', '100');
+		helis = [];
+		for (var i = 0; i < 3; i++) {
+			spawnHeli();
+		}
+		overlay.style.display = 'none';
+		running = true;
+	}
+
+	setInterval(f, 10);
+
+	setInterval(spawnProjectile, 3000);
+
 	setInterval(function() {
 		const delta = (Date.now() - prevMainFrameTime) / 16;
         prevMainFrameTime = Date.now();
 		if (!running) {return;}
 		var p = projectiles.children;
 		for (var i = 0; i < p.length; i++) {
-			var angle = degreeInRadiant * (Number(p[i].dataset.rot) - 90);
+			var angle = degreeInRadiant * (Number(p[i].getAttribute('data-rot')) - 90);
 			var deltaX = speed * Math.cos(angle);
 			var deltaY = speed * Math.sin(angle);
-			p[i].style.top = (parseFloat(p[i].style.top) + deltaY) + 'px';
-			p[i].style.left = (parseFloat(p[i].style.left) + deltaX) + 'px';
+			p[i].style.top = (parseFloat(p[i].style.top) + deltaY * delta) + 'px';
+			p[i].style.left = (parseFloat(p[i].style.left) + deltaX * delta) + 'px';
 			var coords = p[i].getBoundingClientRect();
 			if (p[i].className.indexOf('jet') > -1) { // Jet-projectiles
 				for (var j = 0; j < helis.length; j++) {
@@ -115,7 +141,7 @@ window.addEventListener('load', function() {
 							spawnHeli();
 						}
 						projectiles.removeChild(p[i]);
-						continue;
+						break;
 					}
 				}
 			} else { // Heli-projectiles
@@ -124,11 +150,12 @@ window.addEventListener('load', function() {
 					(coords.top <= coords2.top + coords2.height && coords.top >= coords2.top) &&
 					(coords.left >= coords2.left && coords.left <= coords2.left + coords2.width)
 				) {
-					var h = Number(targetEle.getAttribute('data-health')) - 10;
+					var h = Number(targetEle.getAttribute('data-health')) - 2;
 					lifebar.style.height = (100 - h) + '%';
 					targetEle.setAttribute('data-health', h);
 					if (targetEle.getAttribute('data-health') === '0') {
 						running = false;
+						overlay.style.display = '';
 						gameover.style.display = '';
 						screenEle.style.webkitAnimationPlayState = 'paused';
 						screenEle.style.animationPlayState = 'paused';
@@ -146,6 +173,7 @@ window.addEventListener('load', function() {
 			}
 		}
 	}, 5);
+
 	setInterval(function() {
 		if (!running) {return;}
 		var coords = screenEle.getBoundingClientRect();
@@ -154,27 +182,15 @@ window.addEventListener('load', function() {
 			helis[i][0].style.left = Math.floor(Math.random() * (coords.width - 61)) + 'px';
 		}
 	}, 2000);
-	function reset() {
-		screenEle.style.webkitAnimationPlayState = 'running';
-		screenEle.style.animationPlayState = 'running';
-		gameover.style.display = 'none';
-		killCounter.innerText = '0';
-		projectiles.innerText = '';
-		heliEle.innerText = '';
-		lifebar.style.height = '0%';
-		targetEle.setAttribute('data-health', '100');
-		helis = [];
-		for (var i = 0; i < 3; i++) {
-			spawnHeli();
-		}
-		running = true;
-	}
+
+	// Firing loop
 	setInterval(function() {
-		if (!running) {return;}
-		if (isBtnPressed('A')) {
+		if (running && isBtnPressed('A')) {
 			spawnProjectile2();
 		}
-	}, 500);
+	}, 100);
+
+	// Movement loop
 	setInterval(function() {
 		const delta = (Date.now() - prevMoveFrameTime) / 16;
         prevMoveFrameTime = Date.now();
@@ -187,14 +203,17 @@ window.addEventListener('load', function() {
 		var coords = getComputedStyle(targetEle);
 		if (isBtnPressed('Up')) {
 			targetEle.style.top = Math.max(parseFloat(coords.top) - speed * delta, 0) + 'px';
-		} else if (isBtnPressed('Right')) {
-			targetEle.style.left = Math.min(parseFloat(coords.left) + speed * delta, screenCoords.width - parseFloat(coords.width)) + 'px';
 		} else if (isBtnPressed('Down')) {
 			targetEle.style.top = Math.min(parseFloat(coords.top) + speed * delta, screenCoords.height - parseFloat(coords.height)) + 'px';
+		}
+		if (isBtnPressed('Right')) {
+			targetEle.style.left = Math.min(parseFloat(coords.left) + speed * delta, screenCoords.width - parseFloat(coords.width)) + 'px';
 		} else if (isBtnPressed('Left')) {
 			targetEle.style.left = Math.max(parseFloat(coords.left) - speed * delta, 0) + 'px';
 		}
 	});
+
 	document.getElementById('btn-restart').addEventListener('click', reset);
+
 	reset();
 });
