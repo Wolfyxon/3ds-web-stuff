86f63eef1342161ac5ea3db5d7d53f1556736da3
Wolfyxon <wolfyxon@gmail.com>
2024-04-20 11:23:36 +0200

    Added test sprites

--- games/elmGameTest/elmGameTest.js
+++ games/elmGameTest/elmGameTest.js
@@ -3,4 +3,9 @@ depend("elementGame");
 window.addEventListener("load", function(){
     const scene = new Scene(document.getElementById("scene"));
 
+    const chicken = new Sprite("../../../img/pets/chicken/chicken1.png");
+    scene.addChild(chicken);
+
+    const maxwell = new Sprite("../../img/pets/maxwell/maxwell1.png");
+    scene.addChild(maxwell);
 });
