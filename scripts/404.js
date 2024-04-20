56f4dc39ed4a77b348d226aad93f2749d673722c
Wolfyxon <wolfyxon@gmail.com>
2024-04-20 18:45:43 +0200

    Home page button points to the right location

--- scripts/404.js
+++ scripts/404.js
@@ -1,3 +1,7 @@
 window.addEventListener("load", function() {
+    const homeLink = document.getElementById("back");
 
+    if(window.location.href.indexOf("3ds-web-stuff") !== -1) {
+        homeLink.href = "/3ds-web-stuff";
+    }
 });
