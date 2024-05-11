8aa678b2e7b0bc65da00e3806da656e72a172e4f
magiczocker10 <59317952+magiczocker10@users.noreply.github.com>
2024-05-11 21:07:51 +0200

    Calculator: Add support for DSi

--- other/calculator/calculator.js
+++ other/calculator/calculator.js
@@ -1,30 +1,30 @@
-window.addEventListener("load", function() {
-    const formula = document.getElementById("formula");
-    const result = document.getElementById("result");
+window.onload = function() {
+	const formula = document.getElementById('formula'),
+		result = document.getElementById('result');
 
-    function run() {
-        if(formula.innerText === "") return;
+	function run() {
+		if (formula.innerText === '') return;
 
-        try {
-            result.innerText = eval(formula.innerText);
-        } catch (e){
-            alert(e)
-        }
-    }
+		try {
+			result.innerText = eval(formula.innerText);
+		} catch (e){
+			alert(e)
+		}
+	}
 
-	document.getElementById("pads").addEventListener("click", function(e) {
+	document.getElementById('pads').onclick = function(e) {
 		const target = e.target;
-		if (target.nodeName !== "BUTTON") return;
-		if (target.innerText === "=") run();
+		if (target.nodeName !== 'BUTTON') return;
+		if (target.innerText === '=') run();
 		else formula.innerText += target.innerText;
-	});
+	};
 
-    document.getElementById("btn-clear").addEventListener("click", function() {
-        result.innerText = "";
-        formula.innerText = "";
-    });
+	document.getElementById('btn-clear').onclick = function() {
+		result.innerText = '';
+		formula.innerText = '';
+	};
 
-    document.getElementById("btn-backspace").addEventListener("click", function() {
-        formula.innerText = formula.innerText.slice(0,-1);
-    });
-});
+	document.getElementById('btn-backspace').onclick = function() {
+		formula.innerText = formula.innerText.slice(0, -1);
+	};
+};
