389f19d7908ff4f763ab9541b8e94a84af8e013e
magiczocker10 <59317952+magiczocker10@users.noreply.github.com>
2024-05-11 21:12:26 +0200

    QR: Add support for DSi

--- other/qr/qr.js
+++ other/qr/qr.js
@@ -1,12 +1,12 @@
-window.addEventListener("load", function() {
-    const qr = document.getElementById("qr");
-    const input = document.getElementById("text");
+window.onload = function() {
+	const qr = document.getElementById('qr'),
+		input = document.getElementById('text');
 
-    function generate(str) {
-        qr.src = "http://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + decodeURIComponent(str);
-    }
+	function generate(str) {
+		qr.src = 'http://api.qrserver.com/v1/create-qr-code/?size=200x200&data=' + decodeURIComponent(str);
+	}
 
-    document.getElementById("btn-gen").addEventListener("click", function() {
-        if (input.value.trim().length) generate(input.value);
-    });
-})
+	document.getElementById('btn-gen').onclick = function() {
+		if (input.value.length) generate(input.value);
+	};
+};
