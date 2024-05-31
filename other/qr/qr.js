7e618b10e935d6df2aa3ee0b77d2acfdc12c3857
basic-experimental <das904@icloud.com>
2024-05-31 19:51:49 -0400

    Attempt to get new codes working (1)

--- other/qr/qr.js
+++ other/qr/qr.js
@@ -1,9 +1,18 @@
 window.addEventListener('load', function() {
 	const qr = document.getElementById('qr'),
 		input = document.getElementById('text');
+		codeSelectors = document.getElementsByName('codeSelectRadio');
 
+	function getSelectedCode(){
+		for(var i = 0; i < codeSelectors.length; i++){
+			if(codeSelectors[i].checked){
+				return codeSelectors[i].value;
+			}
+		}
+	}
+	
 	function generate(str) {
-		qr.src = 'http://api.qrserver.com/v1/create-qr-code/?size=200x200&data=' + decodeURIComponent(str);
+		qr.src = 'https://barcodeapi.org/api/' + getSelectedCode() + '/' + decodeURIComponent(str);
 	}
 
 	document.getElementById('btn-gen').addEventListener('click', function() {
