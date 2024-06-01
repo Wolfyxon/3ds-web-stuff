944f362728552b78e0fbc65a2e4789e6f11aa94d
basic-experimental <das904@icloud.com>
2024-06-01 10:26:07 -0400

    Make https

--- other/qr/qr.js
+++ other/qr/qr.js
@@ -38,7 +38,7 @@ window.addEventListener('load', function() {
 
 	
 	function generate(str) {
-		var imageUrl = 'http://barcode.orcascan.com/?type=' + getSelectedCode() + '&data=' + decodeURIComponent(str);
+		var imageUrl = 'https://barcode.orcascan.com/?type=' + getSelectedCode() + '&data=' + decodeURIComponent(str);
 		loadImageAsDataURL(imageUrl, function(dataUrl) {
     			qr.src = dataUrl;
 		});
