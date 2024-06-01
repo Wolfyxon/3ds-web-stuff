8800d545a9d0e39cd35d96166bad1cc6c21cfe48
basic-experimental <das904@icloud.com>
2024-05-31 20:08:44 -0400

    Try to get new codes working (2)

--- other/qr/qr.js
+++ other/qr/qr.js
@@ -10,9 +10,38 @@ window.addEventListener('load', function() {
 			}
 		}
 	}
+
+	function loadImageAsDataURL(url, callback) { // 3DS refuses to wait for api when just setting the src, so I need to request it through JS
+    		var xhr = new XMLHttpRequest();
+    		xhr.open('GET', url, true);
+    		xhr.responseType = 'blob';
+
+    		xhr.onload = function() {
+        		if (this.status === 200) {
+            			var reader = new FileReader();
+            			reader.onloadend = function() {
+                			var base64data = reader.result.replace("data:", "").replace(/^.+,/, "");
+                			callback(base64data);
+            			}
+            			reader.readAsDataURL(this.response);
+        		} else {
+            			console.error("Failed to load image", this.statusText);
+        		}
+    		};
+
+    		xhr.onerror = function() {
+        		console.error("Request failed");
+    		};
+
+   		xhr.send();
+	}
+
 	
 	function generate(str) {
-		qr.src = 'https://barcodeapi.org/api/' + getSelectedCode() + '/' + decodeURIComponent(str);
+		var imageUrl = 'https://barcodeapi.org/api/' + getSelectedCode() + '/' + decodeURIComponent(str);
+		loadImageAsDataURL(imageUrl, function(dataUrl) {
+    			qr.src = dataUrl;
+		});
 	}
 
 	document.getElementById('btn-gen').addEventListener('click', function() {
