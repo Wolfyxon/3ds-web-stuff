44fbbc3e0cd93b965c64406d235ace6a29d68a62
magiczocker10 <59317952+magiczocker10@users.noreply.github.com>
2024-06-02 08:13:01 +0200

    [Font] Add support for DSi

--- other/font/font.js
+++ other/font/font.js
@@ -1,24 +1,20 @@
 window.addEventListener('load', function() {
 	const table = document.getElementById('table');
+	var hRow = '<th>U+</th>',
+		rows = '';
+
+	/* Add header */
+	for (var i=0; i<16; i++) {
+		hRow += '<th>' + (i).toString(16).toUpperCase() + '</th>';
+	}
 
 	/* Add content */
 	for (var y=0; y<8; y++) {
-		const row = table.insertRow(table.rows.length);
-		row.insertCell().textContent = 'E0' + y + 'x';
+		rows += '<tr><td>E0' + y + 'x</td>';
 		for (var x=0; x<16; x++) {
-			const cell = row.insertCell(row.cells.length);
-			cell.textContent = String.fromCharCode('0xE0' + y + (x).toString(16))
+			rows += '<td>' + String.fromCharCode('0xE0' + y + (x).toString(16)) + '</td>';
 		}
+		rows += '</tr>';
 	}
-
-	const header = table.createTHead();
-	const hRow = header.insertRow();
-
-	/* Add header */
-	hRow.insertCell().textContent = 'U+';
-	for (var x=0; x<16; x++) {
-		const hCell = hRow.insertCell(hRow.cells.length);
-		hCell.textContent = (x).toString(16).toUpperCase();
-	}
-});
-
+	table.innerHTML = '<thead><tr>' + hRow + '</tr></thead><tbody>' + rows + '</tbody>';
+}, false);
