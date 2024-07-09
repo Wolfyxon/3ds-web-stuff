61099fa6542a05b09313c2f5d6f6e1bd4d259eec
Wolfyxon <wolfyxon@gmail.com>
2024-07-09 12:00:27 +0200

    Init API lib

---
+++ scripts/lib/api.js
@@ -0,0 +1,17 @@
+// API wrapper for the 3DS Web Stuff API
+
+libName("api");
+depend("net");
+
+const api = {
+    url: "localhost:3000/api" // TODO: Change when the API is deployed to the web
+};
+
+/**
+ * Sends an HTTP request to the 3DS Web Stuff API
+ * @param {string} endpoint
+ * @param {function} callback
+ */
+api.request = function(endpoint, callback) {
+    httpGet(url + "/" + endpoint, callback, true);
+}
